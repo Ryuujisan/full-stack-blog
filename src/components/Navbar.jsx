1dd768c336d7d06d5bf1b9d60a5fc1b3240a7d62
Ryuu <sakurai.ryuu@gmail.com>
2025-07-15 00:04:28 +0200

    nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+
+
+const Navbar = () =>{
+    const[open, setOpen] = useState(false);
+    return (
+        <div className='w-full h-16 md:h-20 flex items-center justify-between'>
+           {/**Logo */}
+           <div className="flex items-center gap-4 text-2xl font-bold">
+
+            <span>lamalog</span>
+           </div>
+           {/**Mobile Menu*/}
+           <div className="md:hidden">
+               <div className="cursor-pointer text-3xl" onClick={() => setOpen(!open)}>
+                   {open ? "X" : "="}
+               </div>
+
+               <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
+                   Menu
+               </div>
+
+           </div>
+           {/**Desktop Menu */}
+           <div className="hidden md:flex items-center gap-8 xl:gab-12 font-medium">
+               <a href="/">Home</a>
+               <a href="/">Trending</a>
+               <a href="/">Most Popular</a>
+               <a href="/">About</a>
+               <a href="/"><button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button></a>
+           </div>
+           {/**Logo */}
+        </div>
+    )
+}
+
+export default Navbar;
